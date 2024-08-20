9be63342deb476b8584d9fee16909e87161926b5
jobs <comepiso8@gmail.com>
2024-08-20 11:04:15 -0400

    modificado

--- RegistroAcademico-master/conn/connection.php
+++ RegistroAcademico-master/conn/connection.php
@@ -1,6 +1,6 @@
 <?php
 try{
-$conn = new PDO('mysql:host=localhost; dbname=centroescolarbd', 'root', 'redes');
+$conn = new PDO('mysql:host=localhost; dbname=centroescolarbd', 'root', 'hola1234');
 } catch(PDOException $e){
    echo "Error: ". $e->getMessage();
    die();
